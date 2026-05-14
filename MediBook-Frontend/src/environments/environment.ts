2caaf4e80116f97d1278d4765439e1b08d39b63e
Abhishek Pathak <abhipathak4546@gmail.com>
2026-05-14 16:54:28 +0530

    [Abhishek Pathak] Refactoring and deploying

--- MediBook-Frontend/src/environments/environment.ts
+++ MediBook-Frontend/src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'http://localhost:5000',
+  gatewayUrl: 'https://api-gateway-vdad.onrender.com',
   apis: {
-    auth: 'http://localhost:5000/api/v1/auth',
-    provider: 'http://localhost:5000/api/v1/Provider',
-    schedule: 'http://localhost:5000/api/v1/Schedule',
-    appointment: 'http://localhost:5000/api/v1/appointments',
-    payment: 'http://localhost:5000/api/v1/payments',
-    review: 'http://localhost:5000/api/v1/reviews',
-    notification: 'http://localhost:5000/api/v1/notifications',
-    record: 'http://localhost:5000/api/v1/records'
+    auth: 'https://api-gateway-vdad.onrender.com/api/v1/auth',
+    provider: 'https://api-gateway-vdad.onrender.com/api/v1/Provider',
+    schedule: 'https://api-gateway-vdad.onrender.com/api/v1/Schedule',
+    appointment: 'https://api-gateway-vdad.onrender.com/api/v1/appointments',
+    payment: 'https://api-gateway-vdad.onrender.com/api/v1/payments',
+    review: 'https://api-gateway-vdad.onrender.com/api/v1/reviews',
+    notification: 'https://api-gateway-vdad.onrender.com/api/v1/notifications',
+    record: 'https://api-gateway-vdad.onrender.com/api/v1/records'
   },
-  signalrHub: 'http://localhost:5006/notifHub',
+  signalrHub: 'https://notification-service-o9q0.onrender.com/notifHub',
   razorpayKey: 'rzp_test_Sgon32WsnbocWD'
 };
